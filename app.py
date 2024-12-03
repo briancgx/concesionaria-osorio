54b47a53b8b365a38d823ce76f718e034ccb4300
Alejandro Daniel Espinosa Chim <119325061+DanielEspinosaChim@users.noreply.github.com>
2024-12-03 00:16:09 -0600

    panel de control

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def login():
         return f"Bienvenido {username}!"
     return render_template("login.html")
 # Ruta principal
+
+@app.route('/panel')
+def panel_control():
+    return render_template('panel_control.html')
+
+@app.route('/clientes')
+def clientes():
+    # Aquí puedes obtener la lista de clientes desde la base de datos
+    return render_template('clientes.html')
+
 @app.route('/')
 def home():
     return render_template('index.html')  # Asegúrate de tener el archivo index.html en la carpeta templates/
